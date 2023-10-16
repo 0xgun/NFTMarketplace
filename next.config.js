d9ed2011ce3fbb4890fdfbaf6b6ab9f2e600c734
Ganeshan M <ganeshanmadesh36@outlook.com>
2023-10-16 16:56:01 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -8,5 +8,6 @@ module.exports = {
     API: process.env.API,
     INFURA_KEY: process.env.INFURA_KEY,
     INFURA_API: process.env.INFURA_API,
+    ALCHEMY_RPC_MUMBAI: process.env.ALCHEMY_RPC_MUMBAI,
   },
 };
