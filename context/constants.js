c988c68712ca4d68551e95d52f2672cfcab38786
Ganeshan M <ganeshanmadesh36@outlook.com>
2023-10-16 15:57:28 +0530

    feat:deploy on sepolia testnet

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,5 @@
 import market from './MarketPlace.json';
 
-export const MarketAddress = '0x209F17CD79334c3430230CF62f7021dDF357d3dD';
+export const MarketAddress = '0xb6860fe7FE439ce4BEb0ba7543bdB27814033AE8';
 
 export const MarketAddressABI = market.abi;
